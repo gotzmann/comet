45fc6b9efc96e29344c44bb08036d128f53bbf25
Serge Gotsuliak <sgotsulyak@gmail.com>
2024-06-30 20:28:09 +0300

    v2.5.1

--- src/Comet.php
+++ src/Comet.php
@@ -19,7 +19,7 @@ use Workerman\Protocols\Http\Response;
  */
 class Comet
 {
-    public const VERSION = '2.5.0';
+    public const VERSION = '2.5.1';
 
     /** @property \Slim\App $app */
     private static $app;
